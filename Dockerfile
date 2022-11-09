b028a10cc7db161591601cb30809caed85221278
Varun Giridhar <32874672+varungiridhar@users.noreply.github.com>
2022-11-08 18:11:07 -0600

    setup environment

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM ros:melodic
+ENTRYPOINT bash
---
+++ code/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/cs225FinalProject2022/code/catkin_ws/src
+# Build directory: /workspaces/cs225FinalProject2022/code/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
