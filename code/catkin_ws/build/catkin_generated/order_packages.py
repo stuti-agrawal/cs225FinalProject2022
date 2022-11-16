cb512c947dd9901faccd544ac4a18690c74654a1
Varun Giridhar <32874672+varungiridhar@users.noreply.github.com>
2022-11-16 05:07:17 +0000

    .gitignore is now working

--- code/catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/workspaces/cs225FinalProject2022/code/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/workspaces/cs225FinalProject2022/code/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/workspaces/cs225FinalProject2022/code/catkin_ws/devel;/opt/ros/melodic' != '' else []
--- code/catkin_ws/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /workspaces/cs225FinalProject2022/code/catkin_ws/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
