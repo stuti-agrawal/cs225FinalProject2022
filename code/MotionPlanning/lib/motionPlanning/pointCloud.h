190f9082169add09be2ed49caf8b619a958e5ba5
Varun Giridhar <32874672+varungiridhar@users.noreply.github.com>
2022-12-02 02:49:17 +0000

    fixed stuff

--- code/MotionPlanning/lib/motionPlanning/pointCloud.h
+++ code/MotionPlanning/lib/motionPlanning/pointCloud.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "lidarPoint.h"
 #include <vector>
 
@@ -43,31 +42,3 @@ private:
     // float pointMin_[3];
     // float pointMax_[3];
 };
-=======
-#include "lidarPoint.h"
-#include <vector>
-
-const unsigned short Dimension = 3;
-const unsigned int DefaultChunkSizeAsPointCount = 2000;
-
-template <typename T>
-class PointCloud {
-public:
-    PointCloud(unsigned int chunkSizeAsPointCount = DefaultChunkSizeAsPointCount);
-    virtual ~PointCloud();
-    const std::vector<LidarPoint<T>> cloud() const;
-    unsigned long numberOfLidarPoints() const;
-    bool pointExists(T x, T y, T z) const;
-    bool addLidarPoint(T x, T y, T z, T r);
-    const T* min() const;
-    const T* max() const;
-private:
-    LidarPoint<T> accessPoint(T x, T y, T z) const;
-public:
-
-private:
-    std::vector<LidarPoint<T>> cloud_;
-    unsigned long pointCount_;
-    T pointMin_[Dimension];
-    T pointMax_[Dimension];
->>>>>>> 098af4acc3a158bc45b81e178d7b41a395a1e19a
