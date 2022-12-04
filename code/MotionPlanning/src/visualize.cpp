bfb74c931f0da1e3e86366da737ff2736de9d649
samdong <hydong__sam@outlook.com>
2022-12-04 00:37:29 +0000

    finished Paint method in visualize

--- code/MotionPlanning/src/visualize.cpp
+++ code/MotionPlanning/src/visualize.cpp
@@ -1,6 +1,7 @@
 #include "visualize.h"
 
 using namespace cs225;
+using namespace std;
 void test() {
     HSLAPixel blackPixel(180, 1, 0);
     PNG visualization_test(100,100);
@@ -15,8 +16,26 @@ void test() {
 void paint() {
     HSLAPixel blackPixel(180, 1, 0);
 
+    PointCloud cloud = readFile("/workspaces/cs225FinalProject2022/data/1616100800400.bin");
+    int min_x = cloud.min()[0];
+    int min_y = cloud.min()[1];
+    int max_x = cloud.max()[0];
+    int max_y = cloud.max()[1];
 
+    int width = max_x - min_x;
+    int height = max_y - min_y;
+    cout << "width" << width << endl;
+    cout << "height" << height << endl;
 
     PNG toReturn(width, height);
-    
+
+    for (LidarPoint l: cloud.cloud()) {
+        int x = l[0] - min_x;
+        int y = l[1] - min_y;
+        
+        cout << x << " " << y << endl;
+        if (x > width || y > height) return;
+        toReturn.getPixel(x, y) = blackPixel;
+    }
+    toReturn.writeToFile("/workspaces/cs225FinalProject2022/data/Painted_Map.png");
 }
--- code/MotionPlanning/src/visualize.h
+++ code/MotionPlanning/src/visualize.h
@@ -1,6 +1,8 @@
 #include "../lib/motionPlanning/HSLAPixel.h"
 #include "../lib/motionPlanning/PNG.h"
-
+#include "../lib/motionPlanning/pointCloud.h"
+#include "filereader.h"
 void test();
 
+// paint the map base on the file imported in this method
 void paint();
